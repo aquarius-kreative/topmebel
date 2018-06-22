bb396b140f43cc1900b4a9e80f84e6f15c33a2aa
akweb <haritonov.aka@gmail.com>
2018-06-23 02:53:50 +0300

    frontpage

--- footer.php
+++ footer.php
@@ -3,40 +3,59 @@
  * @package topmebel.
  */
 ?>
-
-<!--<div class="uk-flex uk-flex-middle uk-flex-auto">-->
-<!--    <div class="tm-vdivider"></div>-->
-<!--    <div class="uk-visible@s uk-text-center tm-border-right">-->
-<!--        <div class="tm-address">-->
-<!--			--><?php //if ( ! empty( get_option( 'address' ) ) ): ?>
-<!--                <span class="tm-icon tmi-location-pin"></span>-->
-<!--				--><?php //echo get_option( 'address' ); ?>
-<!--			--><?php //endif; ?>
-<!--        </div>-->
-<!--        <div class="tm-phones">-->
-<!--			--><?php //if ( ! empty( get_option( 'phone1' ) ) ): ?>
-<!--                <a href="tel:--><?php //echo get_option( 'phone1' ); ?><!--">-->
-<!--                    <span class="tm-icon tmi-iphone"></span>-->
-<!--					--><?php //echo get_option( 'phone1' ); ?>
-<!--                </a>-->
-<!--			--><?php
-//			endif;
-//			if ( ! empty( get_option( 'phone2' ) ) ): ?>
-<!--                <a href="tel:--><?php //echo get_option( 'phone2' ); ?><!--">-->
-<!--                    <span class="tm-icon tmi-iphone"></span>-->
-<!--					--><?php //echo get_option( 'phone2' ); ?>
-<!--                </a>-->
-<!--			--><?php
-//			endif;
-//			if ( ! empty( get_option( 'phone3' ) ) ): ?>
-<!--                <a href="tel:--><?php //echo get_option( 'phone3' ); ?><!--">-->
-<!--                    <span class="tm-icon tmi-iphone"></span>-->
-<!--					--><?php //echo get_option( 'phone3' ); ?>
-<!--                </a>-->
-<!--			--><?php //endif; ?>
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
+<footer class="tm-footer uk-padding uk-padding-remove-horizontal">
+    <div class="uk-container">
+        <div class="uk-flex uk-flex-wrap uk-flex-middle uk-child-width-1-3">
+            <div>
+                <div class="uk-card uk-card-small uk-card-body">
+                    <a href="<?php echo site_url(); ?>" class="uk-navbar-item tm-logo"
+                       title="<?php echo bloginfo( 'name' ); ?>">
+						<?php
+						$custom_logo_id = get_theme_mod( 'custom_logo' );
+						$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+						if ( has_custom_logo() ) {
+							echo '<img src="' . esc_url( $logo[0] ) . '">';
+						} else {
+							echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
+						}
+						?>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-text-center uk-light uk-card-small uk-card-body">
+					<?php if ( ! empty( get_option( 'address' ) ) ): ?>
+                        <p>
+                            <span class="tm-icon tmi-location-pin"></span>
+							<?php echo get_option( 'address' ); ?>
+                        </p>
+					<?php endif; ?>
+					<?php if ( ! empty( get_option( 'phone1' ) ) ): ?>
+                        <p class="uk-margin-remove">
+                            <a href="tel:<?php echo get_option( 'phone1' ); ?>" style="color: white;">
+								<?php echo get_option( 'phone1' ); ?>
+                            </a>
+                        </p>
+					<?php endif; ?>
+	                <?php if ( ! empty( get_option( 'phone2' ) ) ): ?>
+                        <p class="uk-margin-remove">
+                            <a href="tel:<?php echo get_option( 'phone2' ); ?>" style="color: white;">
+				                <?php echo get_option( 'phone2' ); ?>
+                            </a>
+                        </p>
+	                <?php endif; ?>
+	                <?php if ( ! empty( get_option( 'phone3' ) ) ): ?>
+                        <p class="uk-margin-remove">
+                            <a href="tel:<?php echo get_option( 'phone3' ); ?>" style="color: white;">
+				                <?php echo get_option( 'phone3' ); ?>
+                            </a>
+                        </p>
+	                <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -5,10 +5,101 @@
  */
 
 get_header(); ?>
+    <div class="uk-position-relative uk-visible-toggle uk-light" uk-slideshow="autoplay: true; max-height: 790; animation: fade">
+        <ul class="uk-slideshow-items">
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/img/slide.jpg'; ?>" alt="" uk-cover>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/img/slide1.jpg'; ?>" alt="" uk-cover>
+            </li>
+        </ul>
+        <div class="tm-header-fog"></div>
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous
+           uk-slideshow-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
+           uk-slideshow-item="next"></a>
 
-    <div class="tm-frontpage">
+        <div class="uk-position-center">
+            <div class="uk-text-center tm-heading">
+                <h1 class="">Мебель на заказ</h1>
+                <p>Мы знаем, какой должна быть идеальная мебель.
+                    Забудьте о стандартах и шаблонах, только эксклюзивный дизайн, оригинальные решения под запросы и
+                    пожелания клиентов.</p>
+            </div>
+        </div>
+    </div>
+    <div class="tm-frontpage uk-padding uk-padding-remove-horizontal uk-margin-large-bottom">
         <div class="uk-container">
-            <h1>Frontpage</h1>
+            <div class="uk-flex uk-flex-wrap uk-child-width-1-3 uk-grid-small uk-grid-match" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-small uk-card-body">
+                        <h2 class="tm-title-border">Каталог продукции</h2>
+                        <p>Предлагаем шкафы-купе, кухни и спальни, офисную мебель, детские, гордеробные. Любая мебель на
+                            заказ в разных дизайнерских вариациях.</p>
+                    </div>
+                </div>
+                <div>
+                    <a href="#" class="tm-product uk-card-hover uk-position-relative">
+                        <div class="uk-cover-container">
+                            <canvas height="300"></canvas>
+                            <img src="<?php echo get_template_directory_uri() . '/img/slide.jpg'; ?>" alt="" uk-cover>
+                        </div>
+                        <div class="tm-product-head">
+                            <h3>Кухни</h3>
+                            <p class="uk-margin-remove">от 13000 руб/м.п</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="tm-product uk-card-hover uk-position-relative">
+                        <div class="uk-cover-container">
+                            <canvas height="300"></canvas>
+                            <img src="<?php echo get_template_directory_uri() . '/img/slide1.jpg'; ?>" alt="" uk-cover>
+                        </div>
+                        <div class="tm-product-head">
+                            <h3>Шкафы-купе</h3>
+                            <p class="uk-margin-remove">от 12000 руб/м.п</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="tm-product uk-card-hover uk-position-relative">
+                        <div class="uk-cover-container">
+                            <canvas height="300"></canvas>
+                            <img src="<?php echo get_template_directory_uri() . '/img/slide.jpg'; ?>" alt="" uk-cover>
+                        </div>
+                        <div class="tm-product-head">
+                            <h3>Кухни</h3>
+                            <p class="uk-margin-remove">от 13000 руб/м.п</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="tm-product uk-card-hover uk-position-relative">
+                        <div class="uk-cover-container">
+                            <canvas height="300"></canvas>
+                            <img src="<?php echo get_template_directory_uri() . '/img/slide.jpg'; ?>" alt="" uk-cover>
+                        </div>
+                        <div class="tm-product-head">
+                            <h3>Кухни</h3>
+                            <p class="uk-margin-remove">от 13000 руб/м.п</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="tm-product uk-card-hover uk-position-relative">
+                        <div class="uk-cover-container">
+                            <canvas height="300"></canvas>
+                            <img src="<?php echo get_template_directory_uri() . '/img/slide.jpg'; ?>" alt="" uk-cover>
+                        </div>
+                        <div class="tm-product-head">
+                            <h3>Кухни</h3>
+                            <p class="uk-margin-remove">от 13000 руб/м.п</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 

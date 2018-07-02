e455fc7d8e2c63d43e3ee0e8eacbe3ddc1e65ff4
alex <haritonov.aka@gmail.com>
2018-07-02 23:18:14 +0300

    new feature

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,10 +4,11 @@
  */
 
 function topmebel_load_scripts() {
-	wp_enqueue_style( 'style-topmebel', get_template_directory_uri() . '/assets/css/theme.css', array(), '_bld_1530401182741', null );
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_style( 'style-topmebel', get_template_directory_uri() . '/assets/css/theme.css', array(), '_bld_1530483127467', null );
 	wp_enqueue_style( 'tm-icon', get_template_directory_uri() . '/src/fonts/tm-icon/style.css', array(), null, null );
-	wp_enqueue_script( 'app-topmebel', get_template_directory_uri() . '/assets/js/app.topmebel.js', array( 'jquery' ), '_bld_1530401182741', true );
-	wp_enqueue_script( 'vendor-topmebel', get_template_directory_uri() . '/assets/js/vendor.topmebel.js', array( 'jquery' ), '_bld_1530401182741', true );
+	wp_enqueue_script( 'vendor-topmebel', get_template_directory_uri() . '/assets/js/vendor.topmebel.js', array(), '_bld_1530483127467', true );
+	wp_enqueue_script( 'app-topmebel', get_template_directory_uri() . '/assets/js/app.topmebel.js', array(), '_bld_1530483127467', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'topmebel_load_scripts' );
--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,20 @@ const feedbackForm = $('#tm-feedback');
 if (feedbackForm) {
     feedbackForm.on('submit', function (event) {
         event.preventDefault();
+        $('button[type=submit]').disable().html(`<div uk-spinner></div>`);
         const data = {
             action: 'tm_feedback',
             nonce_code: tmajax.nonce,
             data: feedbackForm.serialize()
         }
-        jQuery.post(tmajax.url, data, function (response) {
-            UIkit.notification(response, {status: 'primary'});
+        $.post(tmajax.url, data, function (response) {
             feedbackForm.prepend(`
                 <div class="uk-alert-success" uk-alert>
                     <a class="uk-alert-close" uk-close></a>
                     <p>Ваше сообщение отправлено.</p>
                 </div>
             `);
+            $('button[type=submit]').enable().html('Отправить');
             feedbackForm[0].reset();
         });
     });
